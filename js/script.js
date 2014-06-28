ce377265518039c6afd7a149c1cfe4d7b5fbdd9d
Rahil Momin <rahil.rules.you@gmail.com>
2014-06-28 15:58:57 +0530

    Directional changes - alpha

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ google.maps.visualRefresh = true;
 currentTravelMode=google.maps.TravelMode.WALKING;
 flights=[];
 order=[];
+temp_direction=[];
 function typeIconUrl(type_t) {
 	switch(type_t){
 		case "D":
@@ -40,8 +41,8 @@ function toggler(e){
 
 function initialize() {
 	var mapOptions = {
-		center: new google.maps.LatLng(19.13285,72.915317),
-		zoom: 16,
+		center: new google.maps.LatLng(19.132975, 72.911786),
+		zoom: 15,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		panControl: false,
 		zoomControl: true,
@@ -53,11 +54,11 @@ function initialize() {
 	// google.maps.event.addListener(map, 'click', function(event) {
 	//placeMarker(event.latLng);
 	//});
-	// markerA = new google.maps.Marker({map:map,icon:"icons/letter_a.png",draggable:true});
-	// markerB = new google.maps.Marker({map:map,icon:"icons/letter_b.png",draggable:true});
-	markerA = new google.maps.Marker({map:map,draggable:true});
-	markerB = new google.maps.Marker({map:map,draggable:true});
-	$.get('/api/v1/location/?format=json').success(
+	markerA = new google.maps.Marker({map:map,icon:"icons/letter_s.png",draggable:true});
+	markerB = new google.maps.Marker({map:map,icon:"icons/letter_d.png",draggable:true});
+	//markerA = new google.maps.Marker({map:map,draggable:true});
+	//markerB = new google.maps.Marker({map:map,draggable:true});
+	$.get('/api/api/v1/location/?format=json').success(
 		function(data){
 			data_array=data.objects;
 			markers = [];
@@ -171,6 +172,7 @@ function updateDirections(display){
 
 
 function directionsupdate(full){
+	var direction_val;
 	elem = document.getElementById("directions");
 	var div = document.createElement("div");
 	div.setAttribute("class","direction-step head");
@@ -186,13 +188,28 @@ function directionsupdate(full){
 	elem.appendChild(div);
 	for(i in flights){
 		var con = flights[i];
+			if(i==0){
+				direction_val=""
+			}
+			else{
+			direction_val = temp_direction[i-1];
+			}
 		var divv = document.createElement("div");
 		var namespan = document.createElement("span");
 		var lengthspan = document.createElement("span");
 		namespan.setAttribute("class","pathname")
 		lengthspan.setAttribute("class","lengthspan")
-		namespan.innerHTML = con.polylineName;
-		lengthspan.innerHTML =  parseInt(con.length) + " m";
+		if (direction_val=="Follow") {
+			namespan.innerHTML = direction_val+" to Walk "+/*on : "+con.polylineName+*/"For ";
+			} 
+		else if (direction_val=="" ) {
+			namespan.innerHTML = "Walk "+/*on : "+con.polylineName+*/"For ";
+		}
+		else{
+			namespan.innerHTML = "Take "+direction_val+" turn and Walk "+/*on : "+con.polylineName+*/"For ";
+		};
+		
+		lengthspan.innerHTML = Math.ceil( parseInt(con.length)/72) + " mins";
 		divv.appendChild(namespan);
 		divv.appendChild(lengthspan);
 		divv.setAttribute("data-path",i);
@@ -202,7 +219,7 @@ function directionsupdate(full){
 			flights[i].setOptions({strokeWeight:8,strokeColor: "#DD2222"});
 		}, function(event){
 			var i=parseInt(this.getAttribute("data-path"));
-			flights[i].setOptions({strokeWeight:5,strokeColor: "#AABBCC"});
+			flights[i].setOptions({strokeWeight:5,strokeColor: "#0099FF"});
 		})
 		elem.appendChild(divv);
 	}
@@ -214,30 +231,112 @@ function direct(){
 	for(i in flights){
 		flights[i].setMap(null);
 	}
-
+	var temp_bearing = [];
+	temp_direction = [];
 	//Clear Directions
 	$("#directions").html("");
 	var from_lat = markerA.position.lat();
 	var from_lng = markerA.position.lng();
 	var to_lat = markerB.position.lat();
 	var to_lng = markerB.position.lng();
-	console.log("/etch?from_lat="+from_lat+"&from_lng="+from_lng+"&to_lat="+to_lat+"&to_lng="+to_lng+"&access="+traveler[currentTravelMode]);
-	$.get("/etch?from_lat="+from_lat+"&from_lng="+from_lng+"&to_lat="+to_lat+"&to_lng="+to_lng+"&access="+traveler[currentTravelMode])
+	console.log("/api/etch?from_lat="+from_lat+"&from_lng="+from_lng+"&to_lat="+to_lat+"&to_lng="+to_lng+"&access="+traveler[currentTravelMode]);
+	$.get("/api/etch?from_lat="+from_lat+"&from_lng="+from_lng+"&to_lat="+to_lat+"&to_lng="+to_lng+"&access="+traveler[currentTravelMode])
 	 .success(function(data){
 		var i=0;
         drawPath(data.details["init"].path,data.details["init"].points,data.details["init"].length,data.details["init"].time,data.details["init"]['path_name'],i)
+        //console.log(data.details["init"].points);
+        //console.log(data.details["init"].points[0]);
+        //console.log(data.details["init"].points[data.details["init"].points.length-1]);
+        //console.log( getBearing(data.details["init"].points[0]['k'],data.details["init"].points[0]['A'],data.details["init"].points[data.details["init"].points.length-1]['k'] , data.details["init"].points[data.details["init"].points.length-1]['A'] ) );
+        temp_bearing.push(getBearing(data.details["init"].points[0]['k'],data.details["init"].points[0]['A'],data.details["init"].points[data.details["init"].points.length-1]['k'] , data.details["init"].points[data.details["init"].points.length-1]['A'] ) );
 		for(i in data.array){
 			var segment = data.details[data.array[i]];
 			if(segment != undefined){
                 drawPath(segment.path,segment.points,segment.length,segment.time,segment['path_name'],i+1)
+                //console.log(segment.points[0]);
+                //console.log(segment.points[segment.points.length - 1]);
+                //console.log( getBearing(segment.points[0]['k'],segment.points[0]['A'],segment.points[segment.points.length - 1]['k'],segment.points[segment.points.length - 1]['A']) );
+                temp_bearing.push(getBearing(segment.points[0]['k'],segment.points[0]['A'],segment.points[segment.points.length - 1]['k'],segment.points[segment.points.length - 1]['A']));
 			}
 		}
         drawPath(data.details["fin"].path,data.details["fin"].points,data.details["fin"].length,data.details["init"].time,data.details["fin"]['path_name'],i+1)
+        //console.log(data.details["fin"].points[0]);
+        //console.log(data.details["fin"].points[data.details["fin"].points.length-1]);
+        //console.log( getBearing(data.details["fin"].points[0]['k'],data.details["fin"].points[0]['A'],data.details["fin"].points[data.details["fin"].points.length-1]['k'],data.details["fin"].points[data.details["fin"].points.length-1]['A'] ) );
+        temp_bearing.push(getBearing(data.details["fin"].points[0]['k'],data.details["fin"].points[0]['A'],data.details["fin"].points[data.details["fin"].points.length-1]['k'],data.details["fin"].points[data.details["fin"].points.length-1]['A'] ));
 	 	/*for(var seg in data.details){
                 drawPath(data.details[seg].path,data.details[seg].points,data.details[seg].length,data.details[seg].time,data.details[seg]['path_name'],seg)
                 order = data.array;
         }*/
-		directionsupdate(data.length)
+        console.log(temp_bearing);
+       for(var i=0;i<temp_bearing.length;i++ ){
+       	var diff_val = temp_bearing[i]-temp_bearing[i+1];
+       	if(diff_val<180 && diff_val>-180){
+       		if(diff_val<0){
+       			if(Math.abs(diff_val)>45 && Math.abs(diff_val)<135){
+       				temp_direction.push("Right");
+       			}
+       			else {
+       				temp_direction.push("Follow");
+       			}
+       			console.log(i+" Right "+diff_val);
+       		}
+       		else{
+       			if(Math.abs(diff_val)>45 && Math.abs(diff_val)<135){
+       				temp_direction.push("Left");
+       			}
+       			else {
+       				temp_direction.push("Follow");
+       			}
+       			console.log(i+" left "+diff_val);
+       		}
+       	}
+       	else if (diff_val>180){
+       		diff_val=diff_val-360;
+       		if (diff_val<0) {
+       			if(Math.abs(diff_val)>45 && Math.abs(diff_val)<135){
+       				temp_direction.push("Right");
+       			}
+       			else {
+       				temp_direction.push("Follow");
+       			}
+       			console.log(i+" Else Right "+diff_val);
+       			} 
+       		else {
+       			if(Math.abs(diff_val)>45 && Math.abs(diff_val)<135){
+       				temp_direction.push("Left");
+       			}
+       			else {
+       				temp_direction.push("Follow");
+       			}
+       				console.log(i+" Else left "+diff_val);
+       			}
+       	}
+       	else {
+       		diff_val = diff_val+360;
+       		if (diff_val<0) {
+       			if(Math.abs(diff_val)>45 && Math.abs(diff_val)<135){
+       				temp_direction.push("Right");
+       			}
+       			else {
+       				temp_direction.push("Follow");
+       			}
+       			console.log(i+" Else 2 Right "+diff_val);
+       			} 
+       		else {
+       			if(Math.abs(diff_val)>45 && Math.abs(diff_val)<135){
+       				temp_direction.push("Left");
+       			}
+       			else {
+       				temp_direction.push("Follow");
+       			}
+       				console.log(i+" Else 2 left "+diff_val);
+       			}
+       	}
+
+       }
+       console.log(temp_direction);
+	   directionsupdate(data.length)
 	 })
 }
 
@@ -252,7 +351,7 @@ function drawPath(path,points,length,time,name,init){
         path: locarray,
         geodesic: true,
 
-        strokeColor: '#AABBCC',
+        strokeColor: '#0099FF',
         strokeOpacity: 1.0,
         strokeWeight: 5,
 
@@ -342,3 +441,30 @@ function directWay(origin,destin,waypoints,path) {
 	
 
 }
+
+/* created by akshay */
+
+ function getBearing (src_lat , src_long , dest_lat , dest_long){
+ 	var lat1 = src_lat.toRadians();
+	var lat2 = dest_lat.toRadians();
+	var long2 = dest_long.toRadians();
+	var long1 = src_long.toRadians();
+	var delta_lat = ( dest_lat- src_lat ).toRadians();
+	var delta_long = ( dest_long - src_long ).toRadians();
+ 	var y = Math.sin(long2-long1) * Math.cos(lat2);
+	var x = Math.cos(lat1)*Math.sin(lat2) - Math.sin(lat1)*Math.cos(lat2)*Math.cos(long2-long1);
+	var brng = Math.atan2(y, x).toDegrees();
+	return brng;
+ }
+
+ /** Extend Number object with method to convert numeric degrees to radians */
+if (typeof Number.prototype.toRadians == 'undefined') {
+    Number.prototype.toRadians = function() { return this * Math.PI / 180; }
+}
+
+
+/** Extend Number object with method to convert radians to numeric (signed) degrees */
+if (typeof Number.prototype.toDegrees == 'undefined') {
+    Number.prototype.toDegrees = function() { return this * 180 / Math.PI; }
+}
+
